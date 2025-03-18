3c081fc9e6bb2883fbfb266f5b036cd191aaa2bb
Muhamad Ridwan <mrdwn1812@gmail.com>
2025-03-18 13:19:07 +0700

    fix

--- app/Http/Controllers/OphLogController.php
+++ app/Http/Controllers/OphLogController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\OphLog;
+use App\Models\Ttv;
+use App\Models\kunjungan;
 
 class OphLogController extends Controller
 {
@@ -26,15 +28,77 @@ class OphLogController extends Controller
 
     public function store(Request $request)
     {
-        $serializedData = serialize($request->all());
+        
+        $data = $request->json()->all();
 
-        $log = OphLog::create([
-            'data' => $serializedData,
-        ]);
+        $nik = $data['nik'];
+
+        $kunjunganIds = Kunjungan::whereHas('pasien', function ($query) use ($nik) {
+            $query->where('nik', $nik);
+        })->pluck('id');
+
+        if ($kunjunganIds->isEmpty()) {
+            return response()->json([
+                'message' => 'No kunjungan found for the given NIK'
+            ], 404);
+        }
+
+        $ttv = Ttv::where('kunjungan_id', $kunjunganIds)->first();
+ 
+        if (!$ttv) {
+            $ttv = Ttv::create([
+                'kunjungan_id' => $kunjunganIds->first() 
+            ]);
+        }
+
+        foreach ($data['examinations'] as $examination) {
+            $examinationName = strtoupper($examination['examination_name']);  
+        
+            switch ($examinationName) {
+                case 'BODY TEMPERATURE':
+                    $ttv->temperature = $examination['result'];
+                    break;
+                case 'BLOOD PRESSURE':
+                    $ttv->blood_pressure = $examination['result'];
+                    break;
+                case 'BMI':
+                    $ttv->bmi = $examination['result'];
+                    break;
+                case 'PULSE':
+                    $ttv->pulse = $examination['result'];
+                    break;
+                case 'RESPIRATION':
+                    $ttv->respiration = $examination['result'];
+                    break;
+                case 'OXYGEN SATURATION':
+                    $ttv->oxygen_saturation = $examination['result'];
+                    break;
+                case 'WEIGHT':
+                    $ttv->weight = $examination['result'];
+                    break;
+                case 'HEIGHT':
+                    $ttv->height = $examination['result'];
+                    break;
+                case 'KNEE HEIGHT':
+                    $ttv->knee_height = $examination['result'];
+                    break;
+                case 'SITTING HEIGHT':
+                    $ttv->sitting_height = $examination['result'];
+                    break;
+                case 'ARM SPAN':
+                    $ttv->arm_span = $examination['result'];
+                    break;
+                case 'BMI CATEGORY':
+                    $ttv->bmi_category = $examination['result'];
+                    break;
+            }
+        }
+
+        $ttv->save();
 
         return response()->json([
-            'message' => 'Log saved successfully',
-            'log' => $log
+            'message' => 'TTV updated successfully',
+            'ttv' => $ttv
         ]);
     }
 }
