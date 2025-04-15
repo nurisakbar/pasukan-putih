2166c1f9a35c336b88ddf6b0f4c54dd7f7dd90d0
Muhamad Ridwan <mrdwn1812@gmail.com>
2025-04-15 08:36:13 +0700

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,27 +44,6 @@
                 </div>
                 <!--end::Col-->
                 <div class="col-lg-3 col-6">
-                    <!--begin::Small Box Widget 2-->
-                    <div class="small-box text-bg-success">
-                        <div class="inner">
-                            <h3>{{ $jumlah_kunjungan_selesai }}</h3>
-                            <p>Jumlah Kunjugan Yang Sudah Selesai</p>
-                        </div>
-                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path
-                                d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z">
-                            </path>
-                        </svg>
-                        <a href="#"
-                            class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            More info <i class="bi bi-link-45deg"></i>
-                        </a>
-                    </div>
-                    <!--end::Small Box Widget 2-->
-                </div>
-                <!--end::Col-->
-                <div class="col-lg-3 col-6">
                     <!--begin::Small Box Widget 3-->
                     <div class="small-box text-bg-warning">
                         <div class="inner">
@@ -107,6 +86,26 @@
                         </a>
                     </div>
                     <!--end::Small Box Widget 4-->
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!--begin::Small Box Widget 2-->
+                    <div class="small-box text-bg-success">
+                        <div class="inner">
+                            <h3>{{ $jumlah_kunjungan_selesai }}</h3>
+                            <p>Jumlah Kunjugan Yang Sudah Selesai</p>
+                        </div>
+                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path
+                                d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z">
+                            </path>
+                        </svg>
+                        <a href="#"
+                            class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
+                            More info <i class="bi bi-link-45deg"></i>
+                        </a>
+                    </div>
+                    <!--end::Small Box Widget 2-->
                 </div>
                 <!-- /.Start col -->
             </div>
