a8440a21a8589536637e7d6f2156c2d358126bff
Muhamad Ridwan <mrdwn1812@gmail.com>
2025-04-30 14:49:36 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -48,7 +48,8 @@ class HomeController extends Controller
                 ->leftjoin('villages', 'villages.id', '=', 'pasiens.village_id')
                 ->leftjoin('districts', 'districts.id', '=', 'villages.district_id')
                 ->leftjoin('regencies', 'regencies.id', '=', 'districts.regency_id')
-                ->where('districts.id', $districtId)
+                // ->where('districts.id', $districtId)
+                ->where('pasiens.user_id', $user->id)
                 ->count();
             } else {
                 $data['jumlah_data_sasaran'] = 0;
